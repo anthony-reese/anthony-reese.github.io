cb79fd39cd81913fdf627a541fd133af4418b8be
anthony-reese <anthony-reese@users.noreply.github.com>
2025-06-16 01:58:09 +0000

    deploy: 8a1dfa4f683f4cc80794767a210af9e61c51c1ad

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,32 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1750039088',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/about/',
+      
+        '/projects/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
