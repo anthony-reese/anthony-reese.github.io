48b856357cd04e171c909f3d14fa01b10de0162a
anthony-reese <anthony-reese@users.noreply.github.com>
2025-06-16 02:21:41 +0000

    deploy: 97f4e6bb87a78850b331b3501b206d19211e649e

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1750040115',resources: [
+    cacheName: 'chirpy-1750040500',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
